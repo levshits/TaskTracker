219941e818ae57b0325a8530a2a87c046fd4d814
levshits <levshits@yahoo.com>
2015-05-07 05:52:51 +0300

    DAL for SQL Server

---
+++ TaskTracker/DAL/SQL Server/SqlServerPersonAccessComponent.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using TaskTracker.Models;
+
+namespace TaskTracker.DAL.SQL_Server
+{
+    public class SqlServerPersonAccessComponent:IPersonAccessComponent
+    {
+        private readonly string connectionString;
+
+        public SqlServerPersonAccessComponent()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["TaskTrackerDbConnection"].ConnectionString;
+        }
+
+        public IEnumerable<Person> GetPersons()
+        {
+            List<Person> result = new List<Person>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Select [Id], [FirstName], [LastName], [MiddleName] From [Person]";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(ParseDataSetRow(reader));
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+
+        private Person ParseDataSetRow(SqlDataReader reader)
+        {
+            var person = new Person
+            {
+                Id=reader.GetInt32(0), 
+                FirstName = reader.GetString(1),
+                LastName = reader.GetString(2)
+                };
+            if (!reader.IsDBNull(3))
+            {
+                person.MiddleName = reader.GetString(3);
+            }
+            return person;
+        }
+
+        public Person GetPerson(int id)
+        {
+            Person person = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Select [Id], [FirstName], [LastName], [MiddleName] From [Person] Where [Id]=@Id";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = id;
+                parameter.SqlDbType = System.Data.SqlDbType.Int;
+                command.Parameters.Add(parameter);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        person = ParseDataSetRow(reader);
+                    }
+                }
+            }
+            return person;
+        }
+
+        public void AddPerson(Person person)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Insert Into [Person]  ( [FirstName], [LastName], [MiddleName]) Values (@FirstName, @LastName, @MiddleName)";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                FillQueryParameters(person, command);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private static void FillQueryParameters(Person person, SqlCommand command)
+        {
+            SqlParameter parameter = new SqlParameter();
+            parameter.ParameterName = "@FirstName";
+            parameter.Value = person.FirstName;
+            parameter.SqlDbType = System.Data.SqlDbType.VarChar;
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter();
+            parameter.ParameterName = "@LastName";
+            parameter.Value = person.FirstName;
+            parameter.SqlDbType = System.Data.SqlDbType.VarChar;
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter();
+            parameter.ParameterName = "@MiddleName";
+            parameter.Value = person.FirstName;
+            parameter.SqlDbType = System.Data.SqlDbType.VarChar;
+            command.Parameters.Add(parameter);
+            foreach (SqlParameter param in command.Parameters)
+            {
+                if (param.Value == null)
+                {
+                    param.Value = DBNull.Value;
+                }
+            }
+        }
+
+        public void UpdatePerson(Person person)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Update [Person] Set [FirstName]=@FirstName, [LastName]=@LastName, [MiddleName]=@MiddleName Where [Id]=@Id";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = person.Id;
+                parameter.SqlDbType = System.Data.SqlDbType.Int;
+                command.Parameters.Add(parameter);
+                FillQueryParameters(person, command);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeletePerson(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Delete From [Person] Where [Id]=@Id";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = id;
+                parameter.SqlDbType = System.Data.SqlDbType.Int;
+                command.Parameters.Add(parameter);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ TaskTracker/DAL/SQL Server/SqlServerTaskAccessComponent.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using TaskTracker.Models;
+
+namespace TaskTracker.DAL.SQL_Server
+{
+    public class SqlServerTaskAccessComponent:ITaskAccessComponent
+    {
+        private string connectionString;
+
+        public SqlServerTaskAccessComponent()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["TaskTrackerDbConnection"].ConnectionString; 
+        }
+        
+        public IEnumerable<Task> GetTasks()
+        {
+            List<Task> tasks = new List<Task>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                string query = "Select [Task].[Id], [Name], [Volume], [StartDate], [EndDate], [Status], [FirstName], [LastName], [MiddleName] From [Task] Left Join [Person] on [Task].[ExecutorId]=[Person].[Id]";
+                command.CommandText = query;
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var task = ParseFullDescriptionDatasetRow(reader);
+                            tasks.Add(task);
+                        }
+                    }
+                }
+
+            }
+            return tasks;
+        }
+
+        public Task GetTask(int id)
+        {
+            Task task = null;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = connection.CreateCommand();
+                string query = "Select [Task].[Id], [Name], [Volume], [StartDate], [EndDate], [Status], [FirstName], [LastName], [MiddleName] From [Task] Left Join [Person] On [Task].[ExecutorId]=[Person].[Id] Where [Task].[Id]=@Id";
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = id;
+                parameter.SqlDbType = System.Data.SqlDbType.Int;
+                command.Parameters.Add(parameter);
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        reader.Read();
+                        task = ParseFullDescriptionDatasetRow(reader);
+                        
+
+                    }
+                }
+
+            }
+            return task;
+        }
+
+        private Task ParseFullDescriptionDatasetRow(SqlDataReader reader)
+        {
+            var task = new Task
+            {
+                Id = reader.GetInt32(0),
+                Name = reader.GetString(1),
+                Volume = (float) reader.GetDouble(2),
+                Status = (TaskStatus) reader.GetInt16(5)
+            };
+            if (!reader.IsDBNull(3))
+            {
+                task.StartDate = (DateTime) reader.GetValue(3);
+            }
+            if (!reader.IsDBNull(4))
+            {
+                task.EndDate = (DateTime) reader.GetValue(4);
+            }
+            if (!reader.IsDBNull(6))
+            {
+                task.Executor = new Person
+                {
+                    FirstName = reader.GetString(6),
+                    LastName = reader.GetString(7),
+                    MiddleName = reader.GetString(8)
+                };
+            }
+            return task;
+        }
+
+        public void AddTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query =
+                    "Insert Into [Task] ([Name], [Volume], [StartDate], [EndDate], [Status], [ExecutorId]) Values (@Name, @Volume, @StartDate, @EndDate, @Status, @ExecutorId)";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SetAllQueryParameters(task, command);
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+        public void UpdateTask(Task task)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Update [Task] Set [Name]=@Name, [Volume]=@Volume, [StartDate]=@StartDate, [EndDate]=@EndDate, [Status]=@Status, [ExecutorId]=@ExecutorId Where [Id]=@Id";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter
+                {
+                    ParameterName = "@Id",
+                    Value = task.Id,
+                    SqlDbType = System.Data.SqlDbType.Int
+                };
+                command.Parameters.Add(parameter);
+                SetAllQueryParameters(task, command);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private void SetAllQueryParameters(Task task, SqlCommand command)
+        {
+            SqlParameter parameter = new SqlParameter
+            {
+                ParameterName = "@Name",
+                Value = task.Name,
+                SqlDbType = System.Data.SqlDbType.VarChar
+            };
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter
+            {
+                ParameterName = "@Volume",
+                Value = task.Volume,
+                SqlDbType = System.Data.SqlDbType.Float
+            };
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter
+            {
+                IsNullable = true,
+                ParameterName = "@StartDate",
+                Value = task.StartDate,
+                SqlDbType = System.Data.SqlDbType.Date
+            };
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter
+            {
+                IsNullable = true,
+                ParameterName = "@EndDate",
+                Value = task.EndDate,
+                SqlDbType = System.Data.SqlDbType.Date
+            };
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter
+            {
+                ParameterName = "@Status",
+                Value = task.Status,
+                SqlDbType = System.Data.SqlDbType.TinyInt
+            };
+            command.Parameters.Add(parameter);
+            parameter = new SqlParameter
+            {
+                IsNullable = true,
+                ParameterName = "@ExecutorId",
+                Value = task.ExecutorId,
+                SqlDbType = System.Data.SqlDbType.Int
+            };
+            command.Parameters.Add(parameter);
+            foreach (SqlParameter param in command.Parameters)
+            {
+                if (param.Value == null)
+                {
+                    param.Value = DBNull.Value;
+                }
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "Delete From [Task] Where Id=@Id";
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                SqlParameter parameter = new SqlParameter();
+                parameter.ParameterName = "@Id";
+                parameter.Value = id;
+                parameter.SqlDbType = System.Data.SqlDbType.Int;
+                command.Parameters.Add(parameter);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
